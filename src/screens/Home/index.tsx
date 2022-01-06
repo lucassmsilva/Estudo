e314d1c75725c8dd99cd8926fb49b07534f628f6
Lucas Silva <lucas.silva@piauiconectado.com.br>
2022-01-06 14:40:00 -0400

    test: only a test

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 export function Home() {
-    return(<View>
-        <View style={styles.header}>
-        </View>
+  return (
+    <View>
+      <View style={styles.header}>
+        <Text>Hello Yan</Text>
+      </View>
+
+      <Text>I'm a other text</Text>
     </View>
-
-
-    );
+  );
 }
